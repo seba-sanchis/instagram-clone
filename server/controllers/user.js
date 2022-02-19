f8b027aa3ab22d25512f3d42e61eeaab5569e1a1
seba-sanchis <seba.sanchis@gmail.com>
2022-02-18 22:55:58 -0300

    auth middleware

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,7 +2,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-import User from "../models/user";
+import User from "../models/user.js";
 
 // Controllers
 export const signin = async (req, res) => {
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,15 +2,16 @@
 import express from "express";
 
 import { getPosts, createPost, updatePost, deletePost, likePost } from "../controllers/posts.js";
+import auth from "../middleware/auth.js";
 
 // Initializing router
 const router = express.Router();
 
 // Routes
 router.get("/", getPosts);
-router.post("/", createPost);
-router.patch("/:id", updatePost);
-router.delete("/:id", deletePost);
-router.patch("/:id/likePost", likePost);
+router.post("/", auth, createPost);
+router.patch("/:id", auth, updatePost);
+router.delete("/:id", auth, deletePost);
+router.patch("/:id/likePost", auth, likePost);
 
 export default router;
