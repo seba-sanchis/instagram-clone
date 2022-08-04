97ba76e1724b8c44108c989b3a356e5b3aa977e1
seba-sanchis <seba.sanchis@gmail.com>
2022-08-04 13:26:40 -0300

    styling the form

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -80,8 +80,10 @@ const Navbar = () => {
                 <svg aria-label="New Post"  className="nav-icon-filled" role="img" viewBox="0 0 24 24"><path className="nav-icon-stroke" d="M2 12v3.45c0 2.849.698 4.005 1.606 4.944.94.909 2.098 1.608 4.946 1.608h6.896c2.848 0 4.006-.7 4.946-1.608C21.302 19.455 22 18.3 22 15.45V8.552c0-2.849-.698-4.006-1.606-4.945C19.454 2.7 18.296 2 15.448 2H8.552c-2.848 0-4.006.699-4.946 1.607C2.698 4.547 2 5.703 2 8.552z"></path><line className="nav-icon-stroke" x1="6.545" x2="17.455" y1="12.001" y2="12.001"></line><line className="nav-icon-stroke" x1="12.003" x2="12.003" y1="6.545" y2="17.455"></line></svg>
                </button>
                {toggleForm && (
-                 <div className="form-background" onClick={() => setToggleForm(false)}>
-                   <Form />
+                 <div className="form-container">
+                  {document.body.style.overflow = "hidden"}
+                  <div className="form-background" onClick={() => setToggleForm(false)}></div>
+                  <Form />
                  </div>
               )}
               <Link className="nav-user" to="">
