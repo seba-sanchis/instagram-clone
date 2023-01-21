b6098e23deed0d0e677f4e2215d0b80f6260381d
seba-sanchis <seba.sanchis@gmail.com>
2023-01-21 19:54:48 -0300

    deploy to aws

--- server/index.js
+++ server/index.js
@@ -20,8 +20,12 @@ app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 
+app.get("/", (req, res) => {
+    res.send("social-media server working properly");
+  });
+
 // MongoDB: connection with atlas cloud database
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
